43dc7834c2e4708d6289cd9885fbc34f17b91de4
나제법 <skwpqjq@gmail.com>
2026-05-06 20:05:08 +0900

    chore(Kafka): kafka Producer/Consumer 설정 클래스 추가

---
+++ src/main/java/com/payment/ledger_service/common/config/KafkaConsumerConfig.java
@@ -0,0 +1,72 @@
+package com.payment.ledger_service.common.config;
+
+import com.payment.ledger_service.ledger.domain.PaymentConfirmMessage;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+import org.springframework.kafka.transaction.KafkaTransactionManager;
+import org.springframework.util.backoff.FixedBackOff;
+
+@EnableKafka
+@RequiredArgsConstructor
+@Configuration
+public class KafkaConsumerConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, PaymentConfirmMessage>> paymentConfirmKafkaListenerContainerFactory(
+        KafkaTemplate<String, Object> kafkaTemplate,
+        KafkaTransactionManager<String, Object> kafkaTransactionManager) {
+        ConcurrentKafkaListenerContainerFactory<String, PaymentConfirmMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(paymentConfirmConsumerFactory());
+        factory.getContainerProperties().setKafkaAwareTransactionManager(kafkaTransactionManager);
+
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate,
+            (record, exception) -> {
+                String topic = "ledger-dlt";
+                return new TopicPartition(topic, record.partition());
+            });
+
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 2L));
+
+        factory.setCommonErrorHandler(errorHandler);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, PaymentConfirmMessage> paymentConfirmConsumerFactory() {
+        Map<String, Object> props = consumerConfigs();
+        props.put(JacksonJsonDeserializer.TYPE_MAPPINGS, "PaymentConfirmMessage:com.payment.ledger_service.ledger.domain.PaymentConfirmMessage");
+        props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getConsumer().getAutoOffsetReset());
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, kafkaProperties.getConsumer().getKeyDeserializer());
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, kafkaProperties.getConsumer().getValueDeserializer());
+        return props;
+    }
+
+}
---
+++ src/main/java/com/payment/ledger_service/common/config/KafkaProducerConfig.java
@@ -0,0 +1,45 @@
+package com.payment.ledger_service.common.config;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.DefaultTransactionIdSuffixStrategy;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaProducerConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<String, Object>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory producerFactory() {
+        DefaultKafkaProducerFactory<Object, Object> factory = new DefaultKafkaProducerFactory<>(producerConfigs());
+        factory.setTransactionIdSuffixStrategy(new DefaultTransactionIdSuffixStrategy(1));
+        return factory;
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = kafkaProperties.buildProducerProperties();
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProperties.getProducer().getKeySerializer());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaProperties.getProducer().getValueSerializer());
+        props.put(JacksonJsonSerializer.TYPE_MAPPINGS, "LedgerEventMessage:com.payment.ledger_service.ledger.domain.LedgerEventMessage");
+        props.put(ProducerConfig.ACKS_CONFIG, kafkaProperties.getProducer().getAcks());
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, kafkaProperties.getProducer().getTransactionIdPrefix());
+        return props;
+    }
+
+}
