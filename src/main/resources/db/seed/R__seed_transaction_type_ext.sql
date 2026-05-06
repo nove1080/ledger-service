9a4de6a7aef47bb7573435544e6e979535323c49
나제법 <skwpqjq@gmail.com>
2026-05-07 02:00:25 +0900

    chore(Dummy): 거래 유형 상세 데이터 수정

--- src/main/resources/db/seed/R__seed_transaction_type_ext.sql
+++ src/main/resources/db/seed/R__seed_transaction_type_ext.sql
@@ -1,6 +1,2 @@
 INSERT INTO transaction_type_ext (code, name) VALUES
-    ('ASSET', 'Asset'),
-    ('LIABILITY', 'Liability'),
-    ('EQUITY', 'Equity'),
-    ('REVENUE', 'Revenue'),
-    ('EXPENSE', 'Expense');
+    ('ORDER', 'order')
