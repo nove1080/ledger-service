8b92a16254a1687e76f472c808f8200a8c317c01
나제법 <skwpqjq@gmail.com>
2026-05-06 22:49:03 +0900

    feat(JpaLedgerEntity): JPA 원장 엔티티 구현

---
+++ src/main/java/com/payment/ledger_service/ledger/infrastructure/jpa/entity/JpaLedgerEntity.java
@@ -0,0 +1,41 @@
+package com.payment.ledger_service.ledger.infrastructure.jpa.entity;
+
+import com.payment.ledger_service.common.domain.BaseTimeEntity;
+import com.payment.ledger_service.ledger.domain.AccountType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@ToString(callSuper = true)
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "ledger")
+public class JpaLedgerEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ledger_id", updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_type_code", nullable = false)
+    private AccountType accountType;
+}
+
