405d86764a73a4b6f321c7027dc558a267704556
나제법 <skwpqjq@gmail.com>
2026-06-19 11:19:53 +0900

    feat(repository): LedgerRepository 인터페이스 구현

--- src/main/java/com/payment/ledger_service/ledger/repository/LedgerRepository.java
+++ src/main/java/com/payment/ledger_service/ledger/repository/LedgerRepository.java
@@ -1,6 +1,13 @@
 package com.payment.ledger_service.ledger.repository;
 
+import com.payment.ledger_service.ledger.domain.DoubleEntryLedger;
+import com.payment.ledger_service.ledger.domain.OrderConfirmationLedgers;
+
 public interface LedgerRepository {
 
     boolean existsIdempotencyKey(String idempotencyKey);
+
+    OrderConfirmationLedgers findOrderConfirmationLedgers();
+
+    void saveLedgerTransaction(DoubleEntryLedger doubleEntryLedger);
 }
